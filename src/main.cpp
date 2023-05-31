8d587265305bb514cba391a52bf4ea881210362e
gen1nya <gen1nya@gmail.com>
2023-06-01 01:57:10 +0300

    added button

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <Wire.h>
 #include <DS18B20.h>
 
-#define CYCLE_DELAY 1000
+#define CYCLE_DELAY 100
 #define WARMUP_DELAY 100
 #define PIN_RS 8
 #define PIN_EN 9
@@ -15,24 +15,32 @@
 #define PIN_D7 13
 #define PIN_18b20_WET 3
 #define PIN_18b20_DRY 4
-#define PIN_DHT 2 
-#define DHTTYPE    DHT11
+#define PIN_DHT 1 
+#define PIN_LED 5
+#define PIN_MODE_BUTTON 2
+#define DHTTYPE DHT11
+
+#define HUMIDITY_MODE 1
+#define TEMPERATURE_MODE 0
 
 // function prototype
 float calculateRelativeHumidity(float dryTemperature, float wetTemperature);
+void changeMode();
 
 LiquidCrystal lcd(PIN_RS, PIN_EN, PIN_D4, PIN_D5, PIN_D6, PIN_D7);
 
 DHT_Unified dht(PIN_DHT, DHTTYPE);
 
-sensors_event_t dhtTempEvent;
 sensors_event_t dhtHumEvent;
+float relativeHumidity;
 
 DS18B20 ds_wet(PIN_18b20_WET);
 DS18B20 ds_dry(PIN_18b20_DRY);
 
 float dry_temp;
 float wet_temp;
+
+uint8_t mode = TEMPERATURE_MODE;
 
 void setup () {
   lcd.begin(20, 4);
@@ -41,60 +49,57 @@ void setup () {
   lcd.println("init");  
   
   dht.begin();
+
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_MODE_BUTTON, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PIN_MODE_BUTTON), changeMode, FALLING);
   
   lcd.setCursor(0, 1);
   lcd.println("waiting for data");
   
   _delay_ms(WARMUP_DELAY);
+  digitalWrite(PIN_LED, HIGH);
 }
 
 // printf("T wet %f", wet_temp); doesnt work in proteus project!
 void loop () {
-
-  // read 18b20 sensors
-  wet_temp = ds_wet.getTempC();
-  dry_temp = ds_dry.getTempC();
   
-  // read DHT sensor
-  dht.temperature().getEvent(&dhtTempEvent);
-  dht.humidity().getEvent(&dhtHumEvent);
+  switch (mode) {
+    case TEMPERATURE_MODE:  
+      // read 18b20 sensors
+      wet_temp = ds_wet.getTempC();
+      dry_temp = ds_dry.getTempC();
 
-  // print all data
-  lcd.clear();
+      // print temperature
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(F("T dry: "));
+      lcd.println(dry_temp);  
+
+      lcd.setCursor(0, 1);
+      lcd.print(F("T wet: "));
+      lcd.print(wet_temp);
+
+    break;
+
+    case HUMIDITY_MODE:
+      // read humidity from DHT sensor
+      dht.humidity().getEvent(&dhtHumEvent);
+      // calculate relative humidity
+      float relativeHumidity = calculateRelativeHumidity(dry_temp, wet_temp);
+
+      // print humidity
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(F("H DHT: "));
+      lcd.print(dhtHumEvent.relative_humidity);
+      lcd.setCursor(0, 1);
+      lcd.print(F("Relative: "));
+      lcd.print(relativeHumidity);
   
-  lcd.setCursor(0, 0);
-  lcd.print(F("T dry: "));
-  lcd.println(dry_temp);  
+    break;
+  }
 
-  lcd.setCursor(0, 1);
-  lcd.print(F("T wet: "));
-  lcd.print(wet_temp);
-  
-  lcd.setCursor(0, 2);
-  lcd.print(F("T DHT: "));
-  lcd.print(dhtTempEvent.temperature);
-  
-  lcd.setCursor(0, 3);
-  lcd.print(F("H DHT: "));
-  lcd.print(dhtHumEvent.relative_humidity);
-
-  _delay_ms(CYCLE_DELAY);
-
-  float relativeHumidity = calculateRelativeHumidity(dry_temp, wet_temp);
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(F("T dry: "));
-  lcd.println(dry_temp);  
-
-  lcd.setCursor(0, 1);
-  lcd.print(F("T wet: "));
-  lcd.print(wet_temp);
-
-  lcd.setCursor(0, 2);
-  lcd.print(F("raletive: "));
-  lcd.print(relativeHumidity);
-  
   _delay_ms(CYCLE_DELAY);
 }
 
@@ -104,4 +109,17 @@ float calculateRelativeHumidity(float dryTemperature, float wetTemperature) {
     float saturationVaporPressure = 6.11 * pow(10, (7.5 * dryTemperature / (237.7 + dryTemperature)));
     float relativeHumidity = (vaporPressure / saturationVaporPressure) * 100;
     return relativeHumidity;
+}
+
+/**
+ * external interrup; won't work correctly in the real world (noise)
+*/
+void changeMode() {
+  if (mode == HUMIDITY_MODE) {
+    mode = TEMPERATURE_MODE;
+    digitalWrite(PIN_LED, HIGH);
+  } else {
+    mode = HUMIDITY_MODE;
+    digitalWrite(PIN_LED, LOW);
+  }
 }
