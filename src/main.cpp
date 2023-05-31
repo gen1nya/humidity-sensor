e2998ebdbfbb3f1d291f03e4684842441a4b002e
gen1nya <gen1nya@gmail.com>
2023-05-31 21:35:41 +0300

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,107 @@
-#include "lcd.h"
-#include "lcd.c"
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdio.h>
-#include "DHT.h"
-#include "DHT.c"
+#include <LiquidCrystal.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <DS18B20.h>
 
-#include <ds18b20/ds18b20.h>
+#define CYCLE_DELAY 1000
+#define WARMUP_DELAY 100
+#define PIN_RS 8
+#define PIN_EN 9
+#define PIN_D4 10
+#define PIN_D5 11
+#define PIN_D6 12
+#define PIN_D7 13
+#define PIN_18b20_WET 3
+#define PIN_18b20_DRY 4
+#define PIN_DHT 2 
+#define DHTTYPE    DHT11
 
-int main() {
-  lcd_init();
-  lcd_clear();
-  lcd_set_cursor(0, 0);
-  lcd_printf("ti pidor");
-  _delay_ms(2500);
+// function prototype
+float calculateRelativeHumidity(float dryTemperature, float wetTemperature);
 
-  int16_t dry_temp;
-  int16_t wet_temp;
+LiquidCrystal lcd(PIN_RS, PIN_EN, PIN_D4, PIN_D5, PIN_D6, PIN_D7);
 
-  double dht_temperature[1];
-	double dht_humidity[1];
+DHT_Unified dht(PIN_DHT, DHTTYPE);
 
-	DHT_Setup();
+sensors_event_t dhtTempEvent;
+sensors_event_t dhtHumEvent;
+
+DS18B20 ds_wet(PIN_18b20_WET);
+DS18B20 ds_dry(PIN_18b20_DRY);
+
+float dry_temp;
+float wet_temp;
+
+void setup () {
+  lcd.begin(20, 4);
+  lcd.noAutoscroll();
+  lcd.setCursor(0, 0);
+  lcd.println("init");  
   
-  while(true) {  
-		ds18b20convert(&PORTB, &DDRB, &PINB, ( 1 << 0 ), 0);
-		_delay_ms(1000);
-		ds18b20read(&PORTB, &DDRB, &PINB, ( 1 << 0 ), 0, &dry_temp);
-    lcd_clear();
-    lcd_set_cursor(0, 0);
-    lcd_printf("t1: %d", (float) dry_temp / 16);
+  dht.begin();
+  
+  lcd.setCursor(0, 1);
+  lcd.println("waiting for data");
+  
+  _delay_ms(WARMUP_DELAY);
+}
 
-	  ds18b20convert( &PORTB, &DDRB, &PINB, ( 1 << 0 ), 1);
-		_delay_ms(1000);
-		ds18b20read(&PORTB, &DDRB, &PINB, ( 1 << 0 ), 1, &wet_temp);
-    lcd_set_cursor(0, 1);
-    lcd_printf("t2: %d", (float) wet_temp / 16);
+// printf("T wet %f", wet_temp); doesnt work in proteus project!
+void loop () {
 
-    DHT_Read(dht_temperature, dht_humidity);
-		
-  	switch (DHT_GetStatus())	{
-			case (DHT_Ok):
-        _delay_ms(1000);
-        lcd_clear();
-        lcd_set_cursor(0, 0);
-        lcd_printf("t3: %d, h1: %d", dht_temperature[0], dht_humidity[0]);
-				break;
-			case (DHT_Error_Checksum):
-        lcd_clear();
-        lcd_set_cursor(0, 0);
-        lcd_printf("dht checksum error");
-				break;
-			case (DHT_Error_Timeout):
-        lcd_clear();
-        lcd_set_cursor(0, 0);
-        lcd_printf("dht timeout error");
-				break;
-      default:
-        lcd_clear();
-        lcd_set_cursor(0, 0);
-        lcd_printf("dht unknown");
-        break;
-		}
-				
-		//Sensor needs 1-2s to stabilize its readings
-		_delay_ms(1000);
-  }
+  // read 18b20 sensors
+  wet_temp = ds_wet.getTempC();
+  dry_temp = ds_dry.getTempC();
+  
+  // read DHT sensor
+  dht.temperature().getEvent(&dhtTempEvent);
+  dht.humidity().getEvent(&dhtHumEvent);
 
-  return 0;
+  // print all data
+  lcd.clear();
+  
+  lcd.setCursor(0, 0);
+  lcd.print(F("T dry: "));
+  lcd.println(dry_temp);  
 
+  lcd.setCursor(0, 1);
+  lcd.print(F("T wet: "));
+  lcd.print(wet_temp);
+  
+  lcd.setCursor(0, 2);
+  lcd.print(F("T DHT: "));
+  lcd.print(dhtTempEvent.temperature);
+  
+  lcd.setCursor(0, 3);
+  lcd.print(F("H DHT: "));
+  lcd.print(dhtHumEvent.relative_humidity);
+
+  _delay_ms(CYCLE_DELAY);
+
+  float relativeHumidity = calculateRelativeHumidity(dry_temp, wet_temp);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(F("T dry: "));
+  lcd.println(dry_temp);  
+
+  lcd.setCursor(0, 1);
+  lcd.print(F("T wet: "));
+  lcd.print(wet_temp);
+
+  lcd.setCursor(0, 2);
+  lcd.print(F("raletive: "));
+  lcd.print(relativeHumidity);
+  
+  _delay_ms(CYCLE_DELAY);
+}
+
+/* Слава богу у нас есть нейросети! */
+float calculateRelativeHumidity(float dryTemperature, float wetTemperature) {
+    float vaporPressure = 6.11 * pow(10, (7.5 * wetTemperature / (237.7 + wetTemperature)));
+    float saturationVaporPressure = 6.11 * pow(10, (7.5 * dryTemperature / (237.7 + dryTemperature)));
+    float relativeHumidity = (vaporPressure / saturationVaporPressure) * 100;
+    return relativeHumidity;
 }
